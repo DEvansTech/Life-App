509b194617c15483eb7d22995765f816f28cfec9
SoftwareGuru <wizard.man0308@gmail.com>
2023-11-07 12:06:02 +0700

    landing screen finished minus fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { LandingScreen } from "./screens/landing-screen";
 
 export default function App() {
   return (
-    <View className="flex-1  items-center justify-center bg-white">
-      <Text className="text-red-600">Open up App.js to start working</Text>
-      <StatusBar style="auto" />
+    <View>
+      <LandingScreen />
     </View>
   );
 }
