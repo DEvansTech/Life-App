54b35d0327e7c69423142132257bd96c00c2e4eb
SoftwareGuru <wizard.man0308@gmail.com>
2023-11-07 18:51:40 +0700

    added react navigation to adjust header stylings

--- App.tsx
+++ App.tsx
@@ -1,18 +1,57 @@
+import * as React from "react";
+import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LandingScreen } from "./screens/landing";
-import { EnterCellPhoneScreen } from "./screens/enter-cell-phone";
-import { OtpVerify } from "./screens/otp-verify";
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate("Landing")}
+        title="Go to Landing"
+      />
+      <Button onPress={() => navigation.navigate("OTP")} title="Go to OTP" />
+      <Button
+        onPress={() => navigation.navigate("Cell-Phone")}
+        title="Go to Cell"
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <View>
-        <LandingScreen />
-        {/* <EnterCellPhoneScreen/> */}
-        {/* <OtpVerify /> */}
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="OTP"
+          component={LandingScreen}
+          options={{
+            title: "OTP Verification",
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: "rgb(242, 242, 242)",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Cell-Phone"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
