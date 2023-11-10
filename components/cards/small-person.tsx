45028de73ec4fccb25a0ab0bc1239df1b9fc8534
SoftwareGuru <wizard.man0308@gmail.com>
2023-11-10 16:59:23 +0700

    finished small person card

--- components/cards/small-person.tsx
+++ components/cards/small-person.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export const SmallPersonCard = () => {
   return (
-    <View>
-      <Text>small view</Text>
+    <View className="w-full flex flex-row items-center justify-between bg-white">
+      <View className="flex flex-row items-center">
+        <Image
+          className="w-[36] h-[36] rounded-full"
+          source={require("../../assets/images/person.png")}
+        />
+        <View className="pl-2.5">
+          <Text
+            style={{ fontFamily: "Poppins_600SemiBold" }}
+            className="text-zinc-600 text-sm font-semibold leading-snug"
+          >
+            Anna Marie .{" "}
+            <Text
+              style={{ fontFamily: "Poppins_400Regular", fontSize: 12 }}
+              className="text-zinc-600"
+            >
+              32 min ago
+            </Text>
+          </Text>
+        </View>
+      </View>
+      {/* <FontAwesome name={"chevron-right"} color={"#A7A7A7"} size={14} /> */}
     </View>
   );
 };
